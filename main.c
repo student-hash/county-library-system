fc4c1652f3906f0755947602e63aeb2c7972cc06
student-hash <98327528+student-hash@users.noreply.github.com>
2022-02-07 17:08:05 +0300

    initial commit

---
+++ main.c
@@ -0,0 +1,36 @@
+/*
+county library system
+by kelvin
+Feb 2020
+MIT License
+C99 compiler
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+int menu()[
+    int action;
+        printf("welcome kelvin /n");
+        printf("select an action below/n");
+        printf("1.add new patron /n");
+        printf("2.view all patrons /n");
+        printf("3.add new resource /n");
+        printf("4.view all resource  /n");
+        printf("your selection /n");
+        scanf("%d",&action);
+        return action;
+]
+
+
+
+int main()
+{
+    int action;
+    printf("Hello world!\n");
+    print("county library system/n")
+    print (welcome kelvin/n)
+    action= menu();
+    printf("your selection %d/n");
+    return 0;
+
+}
